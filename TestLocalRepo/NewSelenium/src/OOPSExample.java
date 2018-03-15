0ab1a7c16d4a8b2e858a0316027b4ef488dc86e5
Sumana <Sumana@Raghavendra>
2018-03-15 07:38:46 +0530

    test

---
+++ TestLocalRepo/NewSelenium/src/OOPSExample.java
@@ -0,0 +1,19 @@
+
+public class OOPSExample {
+
+	public static void main(String[] args) {
+	
+		OOPSExample ab1=new OOPSExample();
+		OOPSExample ab2=new OOPSExample();
+		OOPSExample ab3=new OOPSExample();
+		OOPSExample ab4=new OOPSExample();
+		OOPSExample ab5=new OOPSExample();
+
+		
+	}
+public void add(){
+	System.out.println("Add");
+}
+	
+	
+}
